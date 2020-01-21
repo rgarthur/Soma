f6d63c888e3088b85a8314b23dc4348b30dde9c6
Arthur Gaspar <arthur.gspr@gmail.com>
2020-01-21 17:33:43 -0200

    Correcao sasa

--- Soma.cpp
+++ Soma.cpp
@@ -3,13 +3,13 @@ int main(){
 	int x, y;
 	y = 10;
 	printf("Man digita um numero aew:");
-	scanf("d%", &x);
+	scanf("%d", &x);
 	if (x<10){
 	 x = x + y;
 	}
 	else{
 	x = x - y;
 	}
-	printf("teu resultado eh esse men:%d");
+	printf("teu resultado eh esse men:%d",x);
 	return 0;
-}
+}                                            
